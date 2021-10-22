f21df53ac1cc7e24a1107b7b4c142562a6e903bc
Jiaxin Ge <jig099@ucsd.edu>
2021-10-22 11:18:55 -0700

    clean code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,20 +3,11 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
- 
 import Navigation from '../Navigation';
-//import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
-//import PasswordForgetPage from '../PasswordForget';
-//import HomePage from '../Home';
- 
 import * as ROUTES from '../../constants/routes';
 
-// entry page for users
-// <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-// <Route path={ROUTES.HOME} component={HomePage} />
-// <Route exact path={ROUTES.LANDING} component={LandingPage} />
 const App = () => (
   <Router>
     <div>
