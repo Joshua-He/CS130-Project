e1d96dd961773404324550cd32a7ca1c9442c874
Yubo He <joshuahe@Yubos-MacBook-Pro.local>
2021-11-10 13:10:01 -0800

    resolve start issue

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,7 @@
-import firebase from "firebase";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
