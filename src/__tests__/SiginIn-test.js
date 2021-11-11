6c549f3ebdef7cb520627b08164f8fa5c3c66804
Jiaxin Ge <jig099@ucsd.edu>
2021-11-11 12:03:00 -0800

    add adaptor

--- src/__tests__/SiginIn-test.js
+++ src/__tests__/SiginIn-test.js
@@ -5,10 +5,11 @@ import { shallow } from 'enzyme'
 
 import { SignInForm } from '../components/SignIn'
 
+
 describe('SignInForm component test ', () => {
     it('when submit button is clicked, it should call doSiginInWithEmailAndPassword', () => {
         const mockClick = jest.fn()
-        const componentWrapper = shallow(<SignInForm/>);
+        const componentWrapper = shallow(<SignInForm firebase={mockClick}/>);
         componentWrapper.find('.submitButton').at(0).simulate('click')
         expect(mockClick).toHaveBeenCalled();
     })
