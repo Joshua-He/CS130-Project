d7332c9cb6e8ffc2d93324c3bf54dc2f6cb06b37
Jiaxin Ge <jig099@ucsd.edu>
2021-11-21 20:55:26 -0800

    google api

---
+++ src/components/Queue/queueMap.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import {googleMapAPIKey} from '../../configuration';
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function MyComponent() {
+  return (
+    <LoadScript
+      googleMapsApiKey={googleMapAPIKey.apiKey}
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default React.memo(MyComponent)
