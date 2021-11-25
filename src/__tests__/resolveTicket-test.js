e39d56f1eb82e2fe582e8b41ef0ac6d155b5f9ce
yihellen <42916179+yihellen@users.noreply.github.com>
2021-11-24 16:15:29 -0800

    add ticket test

--- src/__tests__/resolveTicket-test.js
+++ src/__tests__/resolveTicket-test.js
@@ -6,7 +6,33 @@ import firebase from 'firebase/app';
 import { withFirebase } from '../components/Firebase';
 import { compose } from 'recompose';
 
-describe('ticket test ', () => {
+describe('ticket test instructor ', () => {
+    let TicketViewTest = class extends TicketView {
+        componentDidMount() { 
+            this.setState({ 
+                ticketData: 
+                    {createdAt: firebase.firestore.Timestamp.now(),
+                    description: "",
+                    isResolved: false,
+                    ownerName: "Jiaxin",
+                    userId: "8vOkiHTn2xTaSFnDoiu7VRwbmUo2",
+                },
+                owned: false
+            })
+        }
+    }
+    const componentWrapper = shallow (<TicketViewTest isinstructor={true}/>)
+    const instance = componentWrapper.instance()
+    const resolveTicketSpy = jest.spyOn(instance,'resolveTicket').mockImplementation(()=>"resolve ticket")
+
+    it('instructor resolve ticket', () => {
+        instance.forceUpdate();
+        componentWrapper.find('Button').simulate('click')
+        expect(resolveTicketSpy).toHaveBeenCalled();
+    });
+})
+
+describe('ticket test student ', () => {
     let TicketViewTest = class extends TicketView {
         componentDidMount() { 
             this.setState({ 
@@ -21,13 +47,20 @@ describe('ticket test ', () => {
             })
         }
     }
-    const componentWrapper = shallow (<TicketViewTest isinstructor={true}/>)
+    const componentWrapper = shallow (<TicketViewTest isinstructor={false}/>)
     const instance = componentWrapper.instance()
     const resolveTicketSpy = jest.spyOn(instance,'resolveTicket').mockImplementation(()=>"resolve ticket")
+    const editTicketSpy = jest.spyOn(instance,'editTicket').mockImplementation(()=>"edit ticket")
 
-    it('instructor resolve ticket', () => {
+    it('student resolve ticket', () => {
         instance.forceUpdate();
-        componentWrapper.find('Button').simulate('click')
+        componentWrapper.find('Button').at(1).simulate('click')
+        expect(resolveTicketSpy).toHaveBeenCalled();
+    });
+
+    it('student edit ticket', () => {
+        instance.forceUpdate();
+        componentWrapper.find('Button').at(0).simulate('click')
         expect(resolveTicketSpy).toHaveBeenCalled();
     });
 })
